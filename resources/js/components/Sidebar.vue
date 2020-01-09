c0a0fe171a88219d73d5b17c698c57fc496aec55
Alexander Pankratov <s.pankratoff@gmail.com>
2020-01-09 04:33:43 +0300

    Sidebar component

---
+++ resources/js/components/Sidebar.vue
@@ -0,0 +1,100 @@
+<template>
+    <v-navigation-drawer
+            v-model="drawer"
+            app
+            :mini-variant="!sideBarHover"
+            :mobile-break-point="1024"
+    >
+        <v-hover
+                :close-delay="300"
+                v-model="sideBarHover"
+        >
+            <div>
+                <v-list-item>
+                    <v-list-item-content>
+                        <v-list-item-title class="title primary--text">
+                            ICA
+                        </v-list-item-title>
+                        <v-list-item-subtitle>
+                            internet content aggregator
+                        </v-list-item-subtitle>
+                    </v-list-item-content>
+                </v-list-item>
+                <v-divider></v-divider>
+                <v-list>
+                    <v-list-item
+                            v-for="item in items"
+                            :key="item.title"
+                            :to="item.path"
+                            v-show="hasAccess(item.access)"
+                            active-class="primary--text"
+                    >
+                        <v-list-item-icon>
+                            <v-icon>{{ item.icon }}</v-icon>
+                        </v-list-item-icon>
+
+                        <v-list-item-content>
+                            <v-list-item-title>{{ item.title }}</v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+                </v-list>
+            </div>
+        </v-hover>
+
+        <template v-slot:append>
+            <v-footer color="transparent" elevation="0">
+                <span class="secondary--text text--lighten-3">&copy; {{new Date().getFullYear()}}</span>
+            </v-footer>
+        </template>
+        <template v-slot:prepend>
+            <v-btn
+                    class="sidebar-controll-button"
+                    fab
+                    color="primary"
+                    top
+                    right
+                    absolute
+                    @click.stop="drawer = !drawer"
+            >
+                <v-icon color="black" v-show="drawer" transition="fade-out-in">mdi-close</v-icon>
+                <v-icon color="black" v-show="!drawer" transition="fade-out-in">mdi-menu</v-icon>
+            </v-btn>
+        </template>
+
+    </v-navigation-drawer>
+</template>
+
+<script>
+    import {mapGetters} from "vuex"
+    import roles from "js/plugins/store/roles"
+
+    export default {
+        name: "Sidebar",
+        data: () => ({
+            drawer: null,
+            sideBarHover: null,
+            items: [
+                {title: "Лента", icon: "mdi-view-headline", path: "/", access: roles.any},
+                {title: "Вход", icon: "mdi-login", path: "/login", access: roles.guest},
+                {title: "Регистрация", icon: "mdi-account", path: "/register", access: roles.guest},
+                {title: "Настройки", icon: "mdi-settings", path: "/settings", access: roles.user},
+                {title: "Выход", icon: "mdi-logout", path: "/logout", access: roles.user},
+            ]
+        }),
+        computed: mapGetters({
+            hasAccess: "user/hasAccess",
+        }),
+    }
+</script>
+
+<style scoped>
+    .v-navigation-drawer {
+        overflow: visible;
+    }
+
+    .v-navigation-drawer .sidebar-controll-button {
+        top: 4px;
+        right: -6em;
+        margin: 12px;
+    }
+</style>
--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,75 +1,12 @@
 <template>
     <v-app id="app">
-        <v-navigation-drawer
-            v-model="drawer"
-            app
-            :mini-variant="!sideBarHover"
-            :mobile-break-point="1024"
-        >
-            <v-hover
-                :close-delay="300"
-                v-model="sideBarHover"
-            >
-                <div>
-                    <v-list-item>
-                        <v-list-item-content>
-                            <v-list-item-title class="title primary--text">
-                                ICA
-                            </v-list-item-title>
-                            <v-list-item-subtitle>
-                                internet content aggregator
-                            </v-list-item-subtitle>
-                        </v-list-item-content>
-                    </v-list-item>
-                    <v-divider></v-divider>
-                    <v-list>
-                        <v-list-item
-                            v-for="item in items"
-                            :key="item.title"
-                            :to="item.path"
-                            v-show="hasAccess(item.access)"
-                            active-class="primary--text"
-                        >
-                            <v-list-item-icon>
-                                <v-icon>{{ item.icon }}</v-icon>
-                            </v-list-item-icon>
-
-                            <v-list-item-content>
-                                <v-list-item-title>{{ item.title }}</v-list-item-title>
-                            </v-list-item-content>
-                        </v-list-item>
-                    </v-list>
-                </div>
-            </v-hover>
-
-            <template v-slot:append>
-                <v-footer color="transparent" elevation="0">
-                    <span class="secondary--text text--lighten-3">&copy; {{new Date().getFullYear()}}</span>
-                </v-footer>
-            </template>
-            <template v-slot:prepend>
-                <v-btn
-                    class="sidebar-controll-button"
-                    fab
-                    color="primary"
-                    top
-                    right
-                    absolute
-                    @click.stop="drawer = !drawer"
-                >
-                    <v-icon color="black" v-show="drawer" transition="fade-out-in">mdi-close</v-icon>
-                    <v-icon color="black" v-show="!drawer" transition="fade-out-in">mdi-menu</v-icon>
-                </v-btn>
-            </template>
-
-        </v-navigation-drawer>
-
+        <sidebar/>
         <notifications/>
 
         <v-content>
             <v-container
-                class="fill-height"
-                fluid
+                    class="fill-height"
+                    fluid
             >
                 <router-view></router-view>
             </v-container>
@@ -79,34 +16,13 @@
 </template>
 
 <script>
-    import {mapActions, mapGetters, mapMutations} from "vuex"
-    import roles from "js/plugins/store/roles"
-    import Notifications from "js/components/Notifications";
+    import Notifications from "js/components/Notifications"
+    import Sidebar from "js/components/Sidebar"
 
     export default {
-        components: {Notifications},
-        data: () => ({
-            drawer: null,
-            sideBarHover: null,
-            items: [
-                {title: "Лента", icon: "mdi-view-headline", path: "/", access: roles.any},
-                {title: "Вход", icon: "mdi-login", path: "/login", access: roles.guest},
-                {title: "Регистрация", icon: "mdi-account", path: "/register", access: roles.guest},
-                {title: "Настройки", icon: "mdi-settings", path: "/settings", access: roles.user},
-                {title: "Выход", icon: "mdi-logout", path: "/logout", access: roles.user},
-            ]
-        }),
-        methods: {
-            ...mapMutations({
-                removeNotification: "notifications/remove",
-                removeAllNotifications: "notifications/removeAll"
-            }),
-            ...mapActions({
-                addNotifications: "notifications/add",
-            }),
+        components: {
+            Notifications,
+            Sidebar
         },
-        computed: mapGetters({
-            hasAccess: "user/hasAccess",
-        }),
     }
 </script>
